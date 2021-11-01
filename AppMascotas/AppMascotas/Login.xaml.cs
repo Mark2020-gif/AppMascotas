c79416625a488f5fcfc8a13a52d53069e40f72ee
Marco Vinicio Allaica Yanchaliquin <marcos-an@outlook.com>
2021-11-01 18:32:47 -0500

    Actualizacion

--- AppMascotas/AppMascotas/Login.xaml.cs
+++ AppMascotas/AppMascotas/Login.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using AppMascotas.Models;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,16 +20,40 @@ namespace AppMascotas
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        public string RequestUri { get; private set; }
+
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            String sUsario = txtUsuario.Text;
-            String sPassword = txtPassword.Text;
+            // String sUsario = txtUsuario.Text;
+            //String sPassword = txtPassword.Text;
 
-            if((sUsario == "Admin") && (sPassword == "1234"))
+            // if((sUsario == "Admin") && (sPassword == "1234"))
+            // {
+            //     Navigation.PushAsync(new Registro());
+            //  }
+
+            sesion log = new sesion
             {
-                Navigation.PushAsync(new Registro());
-            }
+                usuario = txtUsuario.Text,
+                pass = txtPassword.Text
 
+            };
+            //Uri RequestUri = new Uri("http://127.0.0.1/moviles/login.php");
+
+             
+            var request = new HttpRequestMessage();
+            request.RequestUri = new Uri("http://172.27.192.1/moviles/login.php");
+            //var json = JsonConvert.SerializeObject(log);
+            request.Method = HttpMethod.Get;
+            // var contentJaon = new StringContent(json, Encoding.UTF8, "application/json");
+            request.Headers.Add("Accept", "application/json");
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.SendAsync(request);
+            //var response = await client.PostAsync(RequestUri, contentJaon);
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                await Navigation.PushAsync(new Registro());
+            }
         }
     }
 }
---
+++ AppMascotas/AppMascotas/Models/sesion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppMascotas.Models
+{
+    public class sesion
+    {
+        internal string usuario;
+
+        public string Email { get; set; }
+        public string pass { get; set; }
+    }
+}
