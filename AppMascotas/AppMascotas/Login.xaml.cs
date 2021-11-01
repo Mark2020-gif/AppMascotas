869316f5a0763491bbe4ec084654d3ec23e8f3e8
Marco Vinicio Allaica Yanchaliquin <marcos-an@outlook.com>
2021-10-31 20:30:21 -0500

    Agregar archivos de proyecto.

---
+++ AppMascotas/AppMascotas/Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppMascotas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            String sUsario = txtUsuario.Text;
+            String sPassword = txtPassword.Text;
+
+            if((sUsario == "Admin") && (sPassword == "1234"))
+            {
+                Navigation.PushAsync(new Registro());
+            }
+
+        }
+    }
+}
