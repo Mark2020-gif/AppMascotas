79dd8b39d3b37733b967bfae138ddc48d91d23be
Marco Allaica <marcos-an@outlook.com>
2021-11-21 17:47:15 -0500

    UPDATE

--- AppMascotas/AppMascotas/Login.xaml.cs
+++ AppMascotas/AppMascotas/Login.xaml.cs
@@ -18,12 +18,17 @@ namespace AppMascotas
     public partial class Login : ContentPage
     {
         private SQLiteAsyncConnection con;
+        private readonly IGoogleManager _googleManager;
+        GoogleUser GoogleUser = new GoogleUser();
+        public bool IsLogedIn { get; set; }
         public Login()
         {
             InitializeComponent();
             con = DependencyService.Get<DataBase>().GetConnection();
+            _googleManager = DependencyService.Get<IGoogleManager>();
+            /* CheckUserLoggedIn();*/
         }
-
+       
         public static IEnumerable<Usuarios> SELECT_WHERE(SQLiteConnection db, string usuario, string contrasenia)
         {
             return db.Query<Usuarios>("SELECT * FROM Usuarios where Usuario = ? and Contrasenia =?", usuario, contrasenia);
@@ -55,6 +60,42 @@ namespace AppMascotas
         private void Button_Clicked_1(object sender, EventArgs e)
         {
             Navigation.PushAsync(new NuevoRegistro());
+
+        }
+
+        /* private void CheckUserLoggedIn()
+ {
+     _googleManager.Login(OnLoginComplete);
+ }*/
+
+        private void OnLoginComplete(GoogleUser googleUser, string message)
+        {
+            if (googleUser != null)
+            {
+                GoogleUser = googleUser;
+                txtUsuario.Text = GoogleUser.Email;
+                IsLogedIn = true;
+            }
+            else
+            {
+                DisplayAlert("Message", message, "Ok");
+            }
+        }
+
+        private void GoogleLogout()
+        {
+            _googleManager.Logout();
+            IsLogedIn = false;
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            _googleManager.Login(OnLoginComplete);
+            _googleManager.Logout();
+
+
+            txtUsuario.Text = "";
+            
 
         }
     }
