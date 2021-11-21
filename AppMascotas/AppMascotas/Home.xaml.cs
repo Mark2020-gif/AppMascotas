c9a8961ff59b53067605ebe8561db02de3ee5c14
Andrea <miryam_andrea@hotmail.com>
2021-11-20 20:38:46 -0500

    actualizacion

--- AppMascotas/AppMascotas/Home.xaml.cs
+++ AppMascotas/AppMascotas/Home.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AppMascotas.Models;
+using SQLite;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,36 @@ namespace AppMascotas
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Home : ContentPage
     {
+        private SQLiteAsyncConnection con;
         public Home()
         {
             InitializeComponent();
+            con = DependencyService.Get<DataBase>().GetConnection();
+        }
+        
+
+        private void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var DatosRegistro = new Animales{ IdMascota = txtIdMascota.Text, NombreM = txtNombreM.Text, Raza = txtRaza.Text, Sexo = txtSexo.Text , NombreD= txtNombreD.Text, Direccion= txtDireccion.Text };
+                con.InsertAsync(DatosRegistro);
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "ok");
+            }
+          
+            LimpiarFormulario();
+        }
+        void LimpiarFormulario() {
+            txtNombreM.Text = "";
+            txtRaza.Text = "";
+            txtSexo.Text = "";
+            txtNombreD.Text = "";
+            txtDireccion.Text = "";
+            DisplayAlert("Mensaje", "Se Registro correctamente", "Ok");
+        
         }
     }
 }
---
+++ AppMascotas/AppMascotas/Models/Animales.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using SQLite;
+
+namespace AppMascotas.Models
+{
+    public class Animales
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+        [MaxLength(50)]
+
+        public string IdMascota { get; set; }
+        [MaxLength(225)]
+
+        public string NombreM { get; set; }
+        [MaxLength(225)]
+
+        public string Raza { get; set; }
+        [MaxLength(225)]
+        
+        public string Sexo { get; set; }
+        [MaxLength(225)]
+
+        public string NombreD { get; set; }
+        [MaxLength(225)]
+
+        public string Direccion { get; set; }
+       
+    }
+}
