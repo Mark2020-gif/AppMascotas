e8c1a61bb88fea23af7ed8a546fff4a2c7b6c8fa
Marco Allaica <marcos-an@outlook.com>
2021-11-21 09:48:05 -0500

    Acutualizar

--- AppMascotas/AppMascotas/Models/Usuarios.cs
+++ AppMascotas/AppMascotas/Models/Usuarios.cs
@@ -21,4 +21,30 @@ namespace AppMascotas.Models
 
         public string Contrasenia { get; set; }
     }
+
+    public class Animales
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int Id { get; set; }
+        [MaxLength(50)]
+
+        public string IdMascota { get; set; }
+        [MaxLength(225)]
+
+        public string NombreM { get; set; }
+        [MaxLength(225)]
+
+        public string Raza { get; set; }
+        [MaxLength(225)]
+
+        public string Sexo { get; set; }
+        [MaxLength(225)]
+
+        public string NombreD { get; set; }
+        [MaxLength(225)]
+
+        public string Direccion { get; set; }
+
+    }
 }
